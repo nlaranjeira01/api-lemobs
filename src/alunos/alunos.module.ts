acd230c75fa893db44a726186e1bca8969d49917
Nelson <nlaranjeira01@gmail.com>
2020-02-06 10:29:13 -0300

    Added initial 'alunos' service

--- src/alunos/alunos.module.ts
+++ src/alunos/alunos.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AlunosController } from './alunos.controller';
+import { AlunosService } from './alunos.service';
 
 @Module({
-  controllers: [AlunosController]
+  controllers: [AlunosController],
+  providers: [AlunosService],
 })
 export class AlunosModule {}
