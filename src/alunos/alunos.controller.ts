4fd40338bbcb4f1671aecc130cfeaa2b9d4fadc8
Nelson <nlaranjeira01@gmail.com>
2020-02-06 12:48:05 -0300

    Implemented the POST route for creating 'aluno'

--- src/alunos/alunos.controller.ts
+++ src/alunos/alunos.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AlunosService } from './alunos.service';
 import { ApiTags } from '@nestjs/swagger';
 import { Aluno } from './aluno.model';
+import { CreateAlunoDto } from './dto/create-aluno.dto';
 
 @ApiTags('Alunos')
 @Controller('aluno')
@@ -12,4 +13,10 @@ export class AlunosController {
   getAllAlunos(): Aluno[] {
     return this.alunosService.getAllAlunos();
   }
+
+  @Post()
+  createAluno(@Body() createAlunoDto : CreateAlunoDto) : Aluno{
+    return this.alunosService.createAluno(createAlunoDto);
+  }
+  
 }
--- src/alunos/alunos.service.ts
+++ src/alunos/alunos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Aluno } from './aluno.model';
+import { CreateAlunoDto } from './dto/create-aluno.dto';
 
 @Injectable()
 export class AlunosService {
@@ -7,5 +8,21 @@ export class AlunosService {
 
   getAllAlunos(): Aluno[] {
     return this.alunos;
+  }
+
+  createAluno(createAlunoDto : CreateAlunoDto) : Aluno{
+    const {nome, data_nascimento, cpf, nota } = createAlunoDto;
+
+    const aluno : Aluno = {
+        id: this.alunos.length,
+        nome,
+        data_nascimento,
+        cpf,
+        nota, 
+           
+    }
+
+    this.alunos.push(aluno);
+    return aluno;
   }
 }
