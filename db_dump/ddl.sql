0113295d9cf5c56314d239e5f603386eb7a2d9d1
Nelson <nlaranjeira01@gmail.com>
2020-02-07 19:57:51 -0300

    Changed the 'cpf' column in the 'aluno' table to be unique

--- db_dump/ddl.sql
+++ db_dump/ddl.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS aluno (
     id SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     data_nascimento DATE NOT NULL,
-    cpf CHAR(14) NOT NULL,
+    cpf CHAR(14) UNIQUE NOT NULL,
     nota FLOAT NOT NULL
 );
 
