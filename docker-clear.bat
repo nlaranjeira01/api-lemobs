4071340598938f33726f68a3000f089139de99a2
Nelson <nlaranjeira01@gmail.com>
2020-02-09 22:46:48 -0300

    Removed docker-clear.bat

--- docker-clear.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-Rem CUIDADO: APAGA TODAS AS IMAGENS E CONTÊINERES
-FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm %%i
-FOR /f "tokens=*" %%i IN ('docker images --format "{{.ID}}"') DO docker rmi %%i
